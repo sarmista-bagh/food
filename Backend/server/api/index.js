72b56a414315db5dcd4c4f8c187c64b1cd845934
sarmistabagh2 <sarmistabagh2@gmail.com>
2026-06-04 00:12:40 +0530

    changed files for port issue after deploy backend

--- Backend/server/api/index.js
+++
@@ -1,4 +0,0 @@
-import app from "../server.js";
-import serverless from "serverless-http";
-
-export default serverless(app);
--- Backend/server/server.js
+++ Backend/server/server.js
@@ -11,10 +11,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
-}));
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,18 +25,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/checkout", checkoutRoutes);
 app.use("/api/admin", adminRoutes);
 
-// app.get("/", (req, res) => {
-//     res.send("API Running Successfully");
-// });
-
-
 app.get("/", (req, res) => {
     res.json({
         success: true,
-        message: "Backend running"
+        message: "Backend running successfully",
     });
 });
 
-// ❌ NO app.listen()
+const PORT = process.env.PORT || 5000;
 
-export default app;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
