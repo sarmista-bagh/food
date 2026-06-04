acc0b383e0a36cd6b12357c7495242e61be0edfa
sarmistabagh2 <sarmistabagh2@gmail.com>
2026-06-04 17:33:48 +0530

    add restaurant api

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -1,5 +1,46 @@
 
 
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import cookieParser from "cookie-parser";
+
+// import authRoutes from "./routes/authRoutes.js";
+// import checkoutRoutes from "./routes/checkoutRoute.js";
+// import adminRoutes from "./routes/adminRoutes.js";
+// import axios from "axios";
+// dotenv.config();
+
+// const app = express();
+
+// app.use(
+//     cors({
+//         origin: process.env.CLIENT_URL,
+//         credentials: true,
+//     })
+// );
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/checkout", checkoutRoutes);
+// app.use("/api/admin", adminRoutes);
+
+
+
+// app.get("/", (req, res) => {
+//     res.json({
+//         success: true,
+//         message: "Backend running successfully",
+//     });
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -8,7 +49,9 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
 import checkoutRoutes from "./routes/checkoutRoute.js";
 import adminRoutes from "./routes/adminRoutes.js";
-import axios from "axios";
+import restaurantRoutes from "./routes/restaurantRoutes.js";
+import menuRoutes from "./routes/menuRoutes.js";
+
 dotenv.config();
 
 const app = express();
@@ -26,8 +69,8 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/checkout", checkoutRoutes);
 app.use("/api/admin", adminRoutes);
-
-
+app.use("/api/restaurants", restaurantRoutes);
+app.use("/api/menu", menuRoutes);
 
 app.get("/", (req, res) => {
     res.json({
